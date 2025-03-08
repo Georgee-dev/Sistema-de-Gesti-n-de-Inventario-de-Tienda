d88e4f2fb04b59384df4acb2d539b2eaf09e16a2
Georgee-dev <georgedeveloper951@gmail.com>
2025-03-08 17:53:16 -0600

    Creamos la funcionalida para manejar los cliente

---
+++ GestionClientes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GestionClientes
+{
+    public static List<Cliente> clientes = new List<Cliente>(); // Lista de clientes
+
+    // Registra un nuevo cliente
+    public static void RegistrarCliente()
+    {
+        Console.Write("Ingrese el ID del cliente: ");
+        int id = int.Parse(Console.ReadLine());
+        Console.Write("Nombre del cliente: ");
+        string nombre = Console.ReadLine();
+        Console.Write("Dirección: ");
+        string direccion = Console.ReadLine();
+        Console.Write("Número de teléfono: ");
+        string telefono = Console.ReadLine();
+
+        clientes.Add(new Cliente(id, nombre, direccion, telefono));
+        Console.WriteLine("Cliente registrado con éxito.");
+    }
+}
